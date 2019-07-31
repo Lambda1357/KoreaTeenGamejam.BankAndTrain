8ec887588b009cbc898b237905b02503f28b48c5
Junhyeong Bae <bjh6681@outlook.kr>
2019-08-01 07:38:20 +0900

    Add soundmanager in gamemanager

---
+++ BankAndTrain/Assets/Script/Manager/SoundManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField]
+    SoundClip[] audioClips;
+    AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        source.loop = true;
+    }
+
+    public void PlayOneShot(string name)
+    {
+        foreach(var song in audioClips)
+        {
+            if (song.name == name)
+            {
+                source.PlayOneShot(song.clip);
+            }
+        }
+    }
+
+    public void PlayBGM(string name)
+    {
+        foreach (var song in audioClips)
+        {
+            if (song.name == name)
+            {
+                source.clip = song.clip;
+                source.Play();
+            }
+        }
+    }
+}
