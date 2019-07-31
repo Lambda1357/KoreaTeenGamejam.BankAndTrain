9e3eb04141231211f24c159bfa3707fa4273ecda
Junhyeong Bae <bjh6681@outlook.kr>
2019-07-31 16:12:31 +0900

    Refactor Enemy AI

--- BankAndTrain/Assets/Scenes/TestTrain/TestEnemy/EnemyMovement.cs
+++ BankAndTrain/Assets/Scenes/TestTrain/TestEnemy/EnemyMovement.cs
@@ -11,7 +11,11 @@ public class EnemyMovement : MonoBehaviour
     CoverPoint[] coverPoints;
     GameObject player;
 
-    [SerializeField] float maxDistenceForCover;
+    
+    [Range(0.0f, 1.0f), SerializeField]
+    float takeCloseCoverRate;
+    [Range(0.0f, 1.0f), SerializeField]
+    float directAttackRate;
     [SerializeField] float fireDelay;
     [SerializeField] float distanceFromCoverPoint;
     [SerializeField] int minGunFireCount;
@@ -50,53 +54,52 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        bool isInCoverDistence;
-        isInCoverDistence = maxDistenceForCover >= ((agentObject.transform.position - player.transform.position)).magnitude;
-
         switch (status)
         {
             case Status.Find:
-                agent.destination = player.transform.position;
-
-                if (isInCoverDistence)
+                float takeActionValue;
+                takeActionValue = Random.Range(0.0f, 1.0f);
+                if (takeActionValue <= takeCloseCoverRate)
                     status = Status.Hide;
+                else takeActionValue = Random.Range(0.0f, 1.0f);
+                if (takeActionValue <= directAttackRate)
+                    status = Status.Shoot;
                 break;
 
             case Status.Hide:
-                CoverPoint closestPoint = coverPoints[0];
-                foreach(var point in coverPoints)
+                CoverPoint closeistPoint = coverPoints[0];
+                foreach (var point in coverPoints)
                 {
-                    if (point.GetMagnitudeWith(player.transform.position) <
-                        closestPoint.GetMagnitudeWith(player.transform.position) && !point.isUsing) 
-                        closestPoint = point;
+                    if ((point.position - player.transform.position).magnitude < closeistPoint.GetMagnitudeWith(player.transform.position))
+                        closeistPoint = point;
                 }
 
-                Vector3 coverPosit = closestPoint.position;
-                if (closestPoint.position.z > player.transform.position.z)
-                    coverPosit.z += coverZDistence;
-                else
-                    coverPosit.z -= coverZDistence;
+                Vector3 pos = closeistPoint.position;
 
-                agent.destination = coverPosit;
+                float zOffset = player.transform.position.z > agentObject.transform.position.z ? -coverZDistence : coverZDistence;
 
-                if (agent.remainingDistance <= distanceFromCoverPoint)
+                agent.destination = new Vector3(pos.x, pos.y, pos.z + zOffset);
+
+                if (agent.remainingDistance < distanceFromCoverPoint)
                 {
                     curGunfireCount = minGunFireCount;
                     status = Status.Shoot;
-                    curCoverPoint = closestPoint;
                 }
-                
                 break;
 
             case Status.Shoot:
-                curFireDelay -= Time.deltaTime;
-                if (curFireDelay <= 0)
+                curFireDelay += Time.deltaTime;
+
+                agent.destination = player.transform.position;
+                if(curFireDelay >= fireDelay)
                 {
-                    MoveToGunFirePosition();
-                    curFireDelay = fireDelay;
+                    //TODO: fire
+                    Debug.Log("Fire!!");
+                    curGunfireCount--;
+                    curFireDelay = 0;
                 }
 
-                if (!isInCoverDistence && curGunfireCount <= 0) 
+                if (curGunfireCount <= 0)
                     status = Status.Find;
                 break;
         }
@@ -107,16 +110,6 @@ public class EnemyMovement : MonoBehaviour
     void SyncSpriteToAgent()
     {
         sprite.transform.position = agentObject.transform.position;
-    }
-
-    void MoveToGunFirePosition()
-    {
-        Vector3 gunFirePosit;
-        gunFirePosit = curCoverPoint.position;
-        gunFirePosit.Set(gunFirePosit.x > 0 ? gunFirePosit.x - gunFireAimDistence : gunFirePosit.x + gunFireAimDistence, gunFirePosit.y,gunFirePosit.z);
-        agent.destination = gunFirePosit;
-        if (agent.remainingDistance <= 0.1)
-            FireBullet();
     }
 
     void FireBullet()
